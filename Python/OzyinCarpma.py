be825e4d6ae033dcbaa51062a4ececbeabc707a1
Hakan KABASAKAL <hakan.kabasakal@bil.omu.edu.tr>
2015-11-20 08:39:40 +0200

    Update OzyinCarpma.py

--- Python/OzyinCarpma.py
+++ Python/OzyinCarpma.py
@@ -2,6 +2,6 @@ def listeCarp(Liste):
    if len(Liste) == 1:
         return Liste[0]
    else:
-        return Liste[0] + listeCarp(Liste[1:])
+        return Liste[0] * listeCarp(Liste[1:])
 
 print(listeCarp([1,3,5,7,9]))
